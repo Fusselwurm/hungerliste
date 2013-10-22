22fe1bd48c30460df0d20a49fa5410c4e26ddc1c
Moritz Schmidt <mo.schmidt@rebuy.de>
2013-10-22 12:47:29 +0200

    @20m wip: structuring

--- bin/main.js
+++ bin/main.js
@@ -3,60 +3,16 @@
 var
     startPage = 'https://menueweb.hofmann-menue.de/alacarte_33335/MenueTafel.aspx',
     http = require('https'),
+    hofmannExtractor = require('../lib/hofmann-extractor'),
     options = {
         host: 'menueweb.hofmann-menue.de',
         port: '80'
     },
-    phantom = require('node-phantom'),
-	xpathGetAll = function (expression, element) {
-		var xpathresult = document.evaluate(expression, element || document, null, XPathResult.ANY_TYPE);
-		var res = [], tmp;
-		while ((tmp = xpathresult.iterateNext())) {
-			res.push(tmp);
-		}
-		return res;
-	},
-	xpathGetFirst = function (expression, element) {
-		var xpathresult = document.evaluate(expression, element || document, null, XPathResult.ANY_TYPE);
-		return tmp = xpathresult.iterateNext();
-	},
-	addEnergyTotal = function (set) {
-		set.energyTotal = set.energyPerKg * set.massInKg;
-	},
-	printResult = function (set) {
-		console.info((set.energyTotal).toFixed(0) + 'kJ ' + set.name);
-	};
-    returnHofmannTable = function () {
 
-        var
-            //document.evaluate('//*[@id="grdTafel"]/tbody/tr[14]', document, null, XPathResult.ANY_TYPE);
-            rows = xpathGetAll('//tr[@class="clsTableItemBestell"]', document, null, XPathResult.ANY_TYPE),
-            next,
-            res = [];
-
-	rows.forEach(function (row) {
-		try {
-			resultset = {
-				name: xpathGetFirst('.//div[@class="divTafelBezei"]', row).textContent, 
-				energyPerKg: parseInt(xpathGetFirst('.//*[contains(@id,"divBrennwert")]', row).textContent, 10) * 10,
-				massInKg: parseInt(xpathGetFirst('.//*[contains(@id, "Einwaage")]//div[@class="clsNwHeader"]', row).textContent, 10) / 1000
-			};
-			addEnergyTotal(resultset);
-			res.push(resultset);
-		} catch (e) {
-			console.error(e);
-		}
-	});
-	
-	res = res.sort(function (a, b) {
-		return a.energyTotal - b.energyTotal;
-	});
-	
-	res.forEach(printResult);
-	
-        return res;
-    };
-
+    printResult = function (set) {
+        console.info((set.energyTotal).toFixed(0) + 'kJ ' + set.name);
+    },
+    phantom = require('node-phantom');
 
 
 //http.request(options);
@@ -68,7 +24,11 @@ phantom.create(function (err, ph) {
            console.log(status);
 
             setTimeout(function () {
-                page.evaluate(returnHofmannTable, function (err, res) {
+                page.evaluate(hofmannExtractor.getHofmannList, function (err, res) {
+
+                    console.log(arguments);
+                    res.forEach(printResult);
+
                     console.log(res);
 
                     ph.exit();
